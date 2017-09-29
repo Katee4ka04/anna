955c5832b66f601f114b9cd081cea360dcdefba8
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-09-29 16:01:36 +0300

    s

--- index.php
+++ index.php
@@ -7,3 +7,7 @@ $button=$app->add('Button');
 $button->set('Нажми :)');
 $button->icon='instagram';
 $button->link('https://www.instagram.com/');
+$button=$app->add('Button');
+$button->set('Кликай :3');
+$button->icon='vk';
+$buttn->link('https://vk.com/');
