5e35abba993acade723874aacd22a6078bd6dba3
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-09-29 15:37:31 +0300

    katee4ka

--- index.php
+++ index.php
@@ -3,3 +3,6 @@ require 'vendor/autoload.php';
 echo 'by katee4ka';
 $app=new \atk4\ui\App('Добро пожаловать на вебстраничку!');
 $app->initLayout('Centered');
+$button=$app->add('Button');
+$button->set('Нажми :)');
+$button->icon='cat';
