11118bf337b3c29c16c26af1e9dc4a9a412c8667
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 15:45:39 +0300

    lklo

--- katee4ka.php
+++ katee4ka.php
@@ -3,11 +3,8 @@ require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Добро пожаловать на вебстраничку!');
 $app->initLayout('Centered');
 
-$label=$app->add('Label');
-$label->set('Я label:D');
-$label->icon='book';
-$label->addClass('masive purple');
-$label->addDetail('test');
+$label=$app->add(['Lable','Я lable:D','massive purple','detail'=>'ква']);
+
 $button1=$app->add('Button');
 $button1->set('обратно');
 $button1->link('index.php');
