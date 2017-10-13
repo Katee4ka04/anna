85cacb18b9898105267ca24f83ad1604e9fe1f47
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 18:41:35 +0300

    myttre

--- katee4ka.php
+++ katee4ka.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Добро пожаловать на вебстраничку!');
 $app->initLayout('Centered');
 
-$label=$app->add(['Label','Я label:D','massive purple','detail'=>'ква']);
+$label=$app->add(['Label','Я label:D','massive purple','detail'=>'смешарики']);
 
 $button1=$app->add('Button');
 $button1->set('обратно');
@@ -11,3 +11,8 @@ $button1->link('index.php');
 
 $img='http://www.kino-teatr.ru/news/3822/46263.jpg';
 $icon=$app->add(['Image',$img]);
+
+$label=>$app->add(['Label','Теперь ты','big pink','detal'=>'впал в детство']);
+
+$img1='http://multsforkids.ru/data/uploads/personaji/kopatich/smeshariki-kopatich.png';
+$img1=$app->add(['Image',$img]);
