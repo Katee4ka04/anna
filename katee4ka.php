60814b6cf9ce3dc09ebcafb1cb1306bfd34d87db
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 15:49:09 +0300

    vgggg

--- katee4ka.php
+++ katee4ka.php
@@ -3,7 +3,7 @@ require 'vendor/autoload.php';
 $app=new \atk4\ui\App('Добро пожаловать на вебстраничку!');
 $app->initLayout('Centered');
 
-$label=$app->add(['Lable','Я lable:D','massive purple','detail'=>'ква']);
+$label=$app->add(['Label','Я label:D','massive purple','detail'=>'ква']);
 
 $button1=$app->add('Button');
 $button1->set('обратно');
