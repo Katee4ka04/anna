713b5ba81db978d4ab85b998752c07dd29342688
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 15:33:52 +0300

    gthryt

--- katee4ka.php
+++ katee4ka.php
@@ -5,6 +5,8 @@ $app->initLayout('Centered');
 
 $label=$app->add('Label');
 $label->set('Я label:D');
+$label->icon='book';
+$label->addClass('masive purple');
 
 $button1=$app->add('Button');
 $button1->set('обратно');
