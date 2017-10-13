661a9a852c597ca9bdfe5a37853c97e8e3c05b51
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 15:56:14 +0300

    lloll

--- katee4ka.php
+++ katee4ka.php
@@ -8,3 +8,6 @@ $label=$app->add(['Label','Я label:D','massive purple','detail'=>'ква']);
 $button1=$app->add('Button');
 $button1->set('обратно');
 $button1->link('index.php');
+
+$img='http://www.kino-teatr.ru/news/3822/46263.jpg';
+$icon=$app->add(['Image',$img]);
