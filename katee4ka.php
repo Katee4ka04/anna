84b5ccef240ba4ab1ff372b98d1cd924cb673f8d
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-10-13 15:28:07 +0300

    jyj766gui

--- katee4ka.php
+++ katee4ka.php
@@ -6,6 +6,6 @@ $app->initLayout('Centered');
 $label=$app->add('Label');
 $label->set('Я label:D');
 
-$button1=$app>add('Button');
+$button1=$app->add('Button');
 $button1->set('обратно');
 $button1->link('index.php');
