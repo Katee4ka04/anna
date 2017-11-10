196deee2a63ce1deeb00a9cae32d091074bc7946
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-11-10 18:42:23 +0200

    дщдшппд

--- win.php
+++ win.php
@@ -11,3 +11,6 @@ $button2=$app->add('Button');
 $button2->set('Заново');
 $button2->link(['index']);
 $button2->addClass('massive blue');
+
+$img='http://pngimg.com/uploads/golden_cup/golden_cup_PNG14534.png';
+$icon=$app->add(['Image',$img]);
