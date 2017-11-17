dc32b62a5cf378aebc025ce8e7dd1e6996bf8e56
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2017-11-17 15:49:43 +0200

    hghjgj

--- test.php
+++ test.php
@@ -16,9 +16,9 @@ $label=$app->add(['Label',$number,'massive teal']);*/
 $min=$_GET['min'];
 $max=$_GET['max'];
 
-if($max-$min==1){
+if((($max-$min==1)and)isset($_GET['key'])){
   $text=$app->add(['Text','Ой, ты ошибся! Сыграй еще раз!']);
-$button=$app->add(['Button','Попробуй с начала','big red']);
+$button=$app->add(['Button','Попробуй сначала','big red']);
 $button->link(['index']);
 $img='http://99px.ru/sstorage/56/2011/05/10705111544492950.jpg';
 $icon=$app->add(['Image',$img]);
@@ -30,12 +30,12 @@ $icon=$app->add(['Image',$img]);
 $button1=$app->add('Button');
 $button1->set('Мое число больше');
 $button1->addClass('big olive');
-$button1->link(['test','min'=>$mid,'max'=>$max]);
+$button1->link(['test','min'=>$mid,'max'=>$max,'key'=>'key']);
 
 $button2=$app->add('Button');
 $button2->set('Moe число меньше');
 $button2->addClass('big pink');
-$button2->link(['test','min'=>$min,'max'=>$mid]);
+$button2->link(['test','min'=>$min,'max'=>$mid',key'=>'key']);
 
 $button3=$app->add('Button');
 $button3->set('Это оно');
