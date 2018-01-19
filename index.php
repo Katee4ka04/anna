7a5d9ba207d38a07f08cf302a6416f09d4317a03
Katee4ka04 <31989266+Katee4ka04@users.noreply.github.com>
2018-01-19 21:20:16 +0200

    5ht5t5ree

--- index.php
+++ index.php
@@ -3,201 +3,7 @@ require 'vendor/autoload.php';
 $app = new \atk4\ui\App('SnowwA!');
 $app->initLayout("Centered");
 /*
-$button = $app->add('Button');
-$button->set('YouTube');
-$button->icon = 'Image';
-$button->addClass('medium red');
-$button->link('https://www.youtube.com/');
 
-$button = $app->add('Button');
-$button->set('Facebook');
-$button->icon = 'Comments';
-$button->addClass('medium blue');
-$button->link('https://www.facebook.com/');
-
-$button = $app->add('Button');
-$button->set('Aliexpress');
-$button->icon = 'Shopping Bag';
-$button->addClass('medium orange');
-$button->link('https://www.aliexpress.com');
-
-$button= $app->add(['Button','index2']);
-$button->link('index2.php');
-$button->addClass('medium green');
-
-$text=$app->add(['Text','Here can be text']);
-$text->addParagraph('Text');
-
-$a= 0;
-  if ($a==0) {
-    $name='Верно!';
-  }else{
-    $name='Неверно!';
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass('medium green');
-
-$a= 0;
-  if ($a>0) {
-    $name='Верно!';
-    $color='green';
-
-  }else{
-    $name='Неверно!';
-    $color='red';
-
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass($color);
-
-$a= 0;
-  if ($a<0) {
-    $name='Верно!';
-    $color='green';
-  }else{
-    $name='Неверно!';
-    $color='red';
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass($color);
-
-$a= 0;
-  if ($a>=0) {
-    $name='Верно!';
-    $color='green';
-  }else{
-    $name='Неверно!';
-    $color='red';
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass($color);
-
-$a= 0;
-  if ($a<=0) {
-    $name='Верно!';
-    $color='green';
-  }else{
-    $name='Неверно!';
-    $color='red';
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass($color);
-
-$a= tesst;
-  if ($a=='test') {
-    $name='Верно!';
-    $color='green';
-  }else{
-    $name='Неверно!';
-    $color='red';
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass($color);
-
-$a= 0;
-  if ($a!=0) {
-    $name='Верно!';
-    $color='green';
-  }else{
-    $name='Неверно!';
-    $color='red';
-  }
-
-$label=$app->add(['Label',$name]);
-$label->addClass($color);
-
-
-$button=$app->add('Button');
-$button->set('КНОПКА');
-$button->link(['index3','name'=>'Ilja','surname'=>'Polivko']);
-
-$button1=$app->add(['Label','1']);
-$button2=$app->add(['Label','2']);
-$button3=$app->add(['Label','3']);
-$button1=$app->add('Button');
-
-  $text=$app->add(['Text','Чтобы начать игру нажмите кнопку']);
-  $button=$app->add(['Button']);
-  $button->set('Играть');
-  $button->link(['index3','min'=>1,'max'=>100]);
-  $button->addClass('blue');
-
-
-
-$cat1=new Cat;
-$cat1->name='Barsik';
-$cat1->color='Grey';
-$cat1->sex='Boy';
-$cat1->age='10';
-$cat1->AgeCheck();
-$label=$app->add(['Label',$cat1->name.' '.$answer]);
-
-$cat2=new Cat;
-$cat2->name='Vasja';
-$cat2->color='White';
-$cat2->sex='Boy';
-$cat2->age='25';
-$cat2->AgeCheck();
-$label=$app->add(['Label',$cat1->name.' '.$answer]);
-
-$cat3=new Cat;
-$cat3->name='Murka';
-$cat3->color='Brown';
-$cat3->sex='Girl';
-$cat3->age='8';
-$cat3->AgeCheck();
-$label=$app->add(['Label',$cat1->name.' '.$answer]);
-
-$cat4=new Cat;
-$cat4->name='Car';
-$cat4->color='Black';
-$cat4->sex='Boy';
-$cat4->age='11';
-$cat4->AgeCheck();
-$label=$app->add(['Label',$cat1->name.' '.$answer]);
-
-$cat5=new Cat;
-$cat5->name='Taison';
-$cat5->color='Grey';
-$cat5->sex='Boy';
-$cat5->age='6';
-$cat5->AgeCheck();
-$label=$app->add(['Label',$cat1->name.' '.$answer]);
-
-
-$sweet1=new Sweet;
-$sweet1->name='Vaverīte';
-$sweet1->color='green';
-$sweet1->price='7';
-$label=$app->add(['Label',$sweet1->name,$sweet1->color.' massive','detail'=>$sweet1->price,'image'=>'https://www.kontoriabi.ee/wp-content/uploads/2017/03/450-00381.jpg']);
-
-
-$sweet2=new Sweet;
-$sweet2->name='Lukss';
-$sweet2->color='red';
-$sweet2->price='9';
-$label2=$app->add(['Label',$sweet2->name,$sweet2->color.' massive','detail'=>$sweet2->price,'image'=>'http://i.ebayimg.com/thumbs/images/g/-EoAAOSwx2dYENET/s-l225.jpg']);
-
-
-$sweet3= new Sweet;
-$sweet3->name='Laciši';
-$sweet3->color='teal';
-$sweet3->price='5';
-$label3=$app->add(['Label',$sweet3->name,$sweet3->color.' massive','detail'=>$sweet3->price,'image'=>'http://www.laima.lv/wp-content/uploads/2016/05/4750001340235_L-C-TIS-epain-tis-2kg-konf.jpg']);
-
-
-$sweet4= new Sweet;
-$sweet4->name='Serenade';
-$sweet4->color='blue';
-$sweet4->price='8';
-$label4=$app->add(['Label',$sweet4->name,$sweet4->color.' massive','detail'=>$sweet4->price,'image'=>'http://www.la.lv/wp-content/uploads/2013/10/g1/serenade2.gif']);
-*/
 
 /*$db=new
 \atk4\data\Persistence_SQL('mysql:dbname=fdb;host=localhost','root','');
@@ -235,22 +41,22 @@ $clothes=$menu->addMenu('Clothes');
 $toys=$menu->addMenu('Toys');
 $electronics=$menu->addMenu('Electronics');
 
-$clothes->addItem('Shirts');
+$clothes->addItem('Shirts','https://www.dhresource.com/albu_302740708_00-1.0x0/long-sleeve-plaid-shirts-for-men-turn-down.jpg');
 $clothes->addItem('Hoodies','http://i.ebayimg.com/images/i/281912640312-0-1/s-l1000.jpg');
-$clothes->addItem('Jeans');
-$clothes->addItem('Shoes');
-$clothes->addItem('Caps');
-$clothes->addItem('Socks');
+$clothes->addItem('Jeans','http://www.everytimefashion.com/images/1/Jeans-Modern.jpg');
+$clothes->addItem('Shoes','http://h3group.ca/wpfiles/wp-content/uploads/2016/04/kid-LED-shoes-SILVER-model-1-K7L9.jpg');
+$clothes->addItem('Caps','https://sc02.alicdn.com/kf/HTB1KSBSKVXXXXcbXpXXq6xXFXXXe/Custom-sublimation-blank-trucker-cap.jpg');
+$clothes->addItem('Socks','https://images-na.ssl-images-amazon.com/images/I/61KHxR02e6L._SL1005_.jpg');
 
-$toys->addItem('Lego');
-$toys->addItem('Dolls');
-$toys->addItem('Teddy toys');
-$toys->addItem('Transformers');
-$toys->addItem('Cars');
+$toys->addItem('Lego','http://englishbookgeorgia.com/blogebg/wp-content/uploads/2015/01/Lego.jpg');
+$toys->addItem('Dolls','https://i.pinimg.com/736x/40/3d/dd/403ddd434828cdea7d1b516371d398e3--anime-dolls-bjd-dolls.jpg');
+$toys->addItem('Teddy toys','https://www.dhresource.com/0x0s/f2-albu-g1-M00-B6-45-rBVaGVZdFHCACn94AAFKNT-j3uc089.jpg/teddy-bears-plush-toys-gifts-stuffed-plush.jpg');
+$toys->addItem('Transformers','https://images-na.ssl-images-amazon.com/images/I/81PoVVc04-L._SY355_.jpg');
+$toys->addItem('Cars','https://images-na.ssl-images-amazon.com/images/M/MV5BMTg5NzY0MzA2MV5BMl5BanBnXkFtZTYwNDc3NTc2._V1_UY1200_CR90,0,630,1200_AL_.jpg');
 
-$electronics->addItem('Phones');
-$electronics->addItem('Tabs');
-$electronics->addItem('Computers');
-$electronics->addItem('Cases');
-$electronics->addItem('Mouses');
-$electronics->addItem('USB');
+$electronics->addItem('Phones','https://cdn.pocket-lint.com/r/s/1200x/assets/images/120309-phones-buyer-s-guide-best-smartphones-image1-cnxvifjlms.jpg');
+$electronics->addItem('Tabs','https://keddr.com/wp-content/uploads/2014/05/a495652f89a266e1c964aeee80f8d726.jpg');
+$electronics->addItem('Computers','http://aropa.co.uk/wp-content/uploads/2016/08/hd-wallpaper-computer-hdv1.jpg');
+$electronics->addItem('Cases','https://www.dhresource.com/0x0s/f2-albu-g5-M01-24-F0-rBVaI1j9qNSAGu_OAAEGnIPvUr4725.jpg/for-samsung-s7edge-phone-cases-surface-screen.jpg');
+$electronics->addItem('Mouses','https://images-na.ssl-images-amazon.com/images/I/817L8yUzMgL._SL1500_.jpg');
+$electronics->addItem('USB','https://mochieng.files.wordpress.com/2013/09/usb-stick-1280px-922px0.jpg');
